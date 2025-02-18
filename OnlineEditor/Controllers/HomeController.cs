1ec8fb3fb0d9cccf5df559b1778fceaeec680cd1
Saif Medhat <seifmedhatfarouk@gmail.com>
2025-02-18 02:41:44 +0200

    feat: add Home page

---
+++ OnlineEditor/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEditor.Models;
+
+namespace OnlineEditor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        
+    }
+}
